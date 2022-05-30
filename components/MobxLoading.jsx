e005b0e29ecc3596d813f34be1d99946d755e167
Abhay Ramesh <ramesh.abhay14@gmail.com>
2022-05-30 19:47:04 +0530

    im dom=ne

--- components/MobxLoading.jsx
+++ components/MobxLoading.jsx
@@ -8,7 +8,7 @@ function MobxLoading() {
                 <div className="w-full max-w-sm justify-center" >
                     <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 opacity-70  ">
                         <div className="flex justify-between items-center space-x-8 ">
-                            <p className="text-gray-700 text-sm font-bold overflow-clip w-44 h-5 bg-slate-300 rounded-sm opacity-40 " ></p>
+                            <p className="text-gray-700 text-sm font-bold overflow-clip w-40 h-5 bg-slate-300 rounded-sm opacity-40 " ></p>
                             <div className="flex items-center space-x-4">
                                 {/* <div className="form-check">
                                     <input className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 my-1 align-top bg-no-repeat bg-center bg-contain float-left cursor-pointer"
@@ -26,7 +26,7 @@ function MobxLoading() {
                 <div className="w-full max-w-sm justify-center" >
                     <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 opacity-70 ">
                         <div className="flex justify-between items-center space-x-8 ">
-                            <p className="text-gray-700 text-sm font-bold overflow-clip w-44 h-5 bg-slate-300 rounded-sm opacity-40 " ></p>
+                            <p className="text-gray-700 text-sm font-bold overflow-clip w-40 h-5 bg-slate-300 rounded-sm opacity-40 " ></p>
                             <div className="flex items-center space-x-4">
                                 {/* <div className="form-check">
                                     <input className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 my-1 align-top bg-no-repeat bg-center bg-contain float-left cursor-pointer"
@@ -44,7 +44,7 @@ function MobxLoading() {
                 <div className="w-full max-w-sm justify-center" >
                     <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 opacity-70 ">
                         <div className="flex justify-between items-center space-x-8 ">
-                            <p className="text-gray-700 text-sm font-bold overflow-clip w-44 h-5 bg-slate-300 rounded-sm opacity-40 " ></p>
+                            <p className="text-gray-700 text-sm font-bold overflow-clip w-40 h-5 bg-slate-300 rounded-sm opacity-40 " ></p>
                             <div className="flex items-center space-x-4">
                                 {/* <div className="form-check">
                                     <input className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 my-1 align-top bg-no-repeat bg-center bg-contain float-left cursor-pointer"
